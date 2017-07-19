25b2880d7f52fc8ee8d78aa1a83afc71901eae5b
Simon Lischka <simon@a-studios.org>
2017-07-19 22:12:08 +0200

    Add Player facade type

--- js/project/plugins.sbt
+++ js/project/plugins.sbt
@@ -1,1 +1,2 @@
 //addSbtPlugin("scalajs-react-interface" % "sbt-sri-platform" % "2017.4.30-beta")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalajs-bundler" % "0.7.0")
---
+++ js/src/main/scala/co/lischka/musiclist/facades/Player.scala
@@ -0,0 +1,43 @@
+package co.lischka.musiclist.facades
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSImport, JSGlobal}
+import scala.scalajs.js.annotation.JSImport.Namespace
+
+//"react-native-streaming-audio-player
+
+@JSImport("node-uuid/uuid", Namespace)
+@js.native
+object player extends Player
+
+@js.native
+trait Player extends js.Object {
+
+  def play(streamUrl: String, metadata: js.Dictionary[String]): Unit = js.native
+
+  def pause(): Unit = js.native
+
+  def resume(): Unit = js.native
+
+  def stop(): Unit = js.native
+
+  def seekTo(timeMillis: Int): Unit = js.native
+
+
+  // These methods are not present in IOS version, maybe should be implemented
+  //
+  //  def isPlaying(Callback cb) {
+  //    cb.invoke(mService.getPlayback().isPlaying());
+  //  }
+  //
+  //  @ReactMethod
+  //  public void getDuration(Callback cb) {
+  //    cb.invoke(mService.getPlayback().getDuration());
+  //  }
+  //
+  //  @ReactMethod
+  //  public void getCurrentPosition(Callback cb) {
+  //    cb.invoke(mService.getPlayback().getCurrentPosition());
+  //  }
+
+}
